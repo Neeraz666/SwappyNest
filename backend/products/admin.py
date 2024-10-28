30e0644d7150ac79365f3262bb53137c194b45a4
Niraj Pandey <neeraz.pandey2058@gmail.com>
2024-10-28 12:02:54 +0545

    Admin of product modified.

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -3,9 +3,9 @@ from .models import Product
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('')
-    list_display_links = ('',)
-    search_fields = ('')
+    list_display = ('productname', 'category', 'purchaseyear',)
+    list_display_links = ('productname',)
+    search_fields = ('productname', 'category', 'condition',)
     list_per_page = 50
 
 admin.site.register(Product, ProductAdmin)
