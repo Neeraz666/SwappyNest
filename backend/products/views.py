79461fee9113511dd6df67e3c3a5d5ae37b06470
Niraj Pandey <neeraz.pandey2058@gmail.com>
2024-11-11 16:39:21 +0545

    Comments added in products.views.

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,11 +8,14 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .serializers import ProductSerializer
 
 # Create your views here.
+
 class UploadProduct(APIView):
+    # UploadProduct function uses APIView and the user should be authenticated (permissions.IsAuthenticated)
     permission_classes = (permissions.IsAuthenticated,)
 
     def post(self, request):
         try:
+            # Current user is retrieved and the data coming from the frontend is stored in productdata
             currentuser = request.user
             productdata = self.request.data
 
@@ -23,6 +26,7 @@ class UploadProduct(APIView):
             condition = productdata['condition']
             category = productdata['category']
 
+            # The data retrieved from the frontend is saved to the Product model
             product = Product(
                 user = currentuser,
                 productname = productname, 
@@ -34,12 +38,17 @@ class UploadProduct(APIView):
             )
             
             product.save()
-
+            
+            # If the product is successfully saved the following response will be returned else error will be returned
             return Response({'success': 'Your product has been sucessfully uploaded.'})
         except Exception as e:
             return Response({'error': e})
         
 class ListAllProduct(ListAPIView):
+    """
+        ListAllProduct uses ListAPIView to list the products
+        The function doesn't need authentication to be called and the products are sent as per the 'id' in ascending order
+    """
     permission_classes = (permissions.AllowAny, )
     queryset = Product.objects.all().order_by('id')
     serializer_class = ProductSerializer
