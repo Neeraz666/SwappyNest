116fd7d42fbeb5899dad35e2779a8151fefdc85f
Niraj Pandey <neeraz.pandey2058@gmail.com>
2024-11-21 21:01:11 +0545

    Changed the number of search results.

--- backend/products/views.py
+++ backend/products/views.py
@@ -111,10 +111,12 @@ class ProductSearchView(ListAPIView):
         # Serialize the products to return
         serialized_products = self.serializer_class([product for product, _ in top_products], many=True)
 
-        return Response(serialized_products.data)
+        return Response(serialized_products.data[10])
     
     def compute_cosine_similarity(self, vec1, vec2):
         """
         Computes cosine similarity between two vectors.
         """
         return cosine_similarity(vec1, vec2)[0][0]
+    
+
